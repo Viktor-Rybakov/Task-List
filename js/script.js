0517721f907562883872acab9491d40a87fdd9e1
ViktorRybakov <viktor.rybakov@gmail.com>
2019-09-27 10:41:21 +0300

    Добавил очистку формы

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 $(function(){
 
   $('.add-task-button').on('click', function(){
+
+    //Записываю содержимое полей в переменные
     var task_name = $('#task-name__field').val();
     var task_description = $('#task-description__field').val();
   
+    //Скрываю пустой лист
     $('.empty-list').addClass('hidden');
-
+    
+    //Генерирую элементы HTML
     $('.task-list').append('<li class="task-list__item"></li>');
     $('.task-list__item').last().append('<div class="task-item"></div>');
     $('.task-item').last().append('<div class="task-item__name-container flex-container"></div>');
     $('.task-item__name-container.flex-container').last().append('<div class="flex-container"></div>');
     $('.flex-container').last().append('<div class="task-item__name"></div>');
-    $('.task-item__name').last().text(task_name);
     $('.flex-container').last().append('<button class="task-item__clear" type="button" aria-label="Удалить дело"></button>');
     $('.task-item__name-container.flex-container').last().append('<button class="task-item__open" type="button" aria-label="Раскрыть дело"></button>');
     $('.task-item').last().append('<div class="task-item__description"></div>');
+
+    //Вставляю содержимое полей в сгенерированные элементы
+    $('.task-item__name').last().text(task_name);
     $('.task-item__description').last().text(task_description);
+
+    //Очищаю форму
+    $('#task-name__field').val('');
+    $('#task-description__field').val('');
     
   });
 
