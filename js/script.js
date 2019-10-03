477fef097c686ba0f9b4d48d4d6914fde8a17e4f
ViktorRybakov <viktor.rybakov@gmail.com>
2019-10-03 17:22:48 +0300

    Вынес стили в отдельные файлы Исправил разметку Переписал код JS: сохранение элементов в переменные, цепочки append

--- js/script.js
+++ js/script.js
@@ -1,47 +1,46 @@
 $(function(){
 
-  $('.add-task-button').on('click', function(){
+  var task_form_Elem = $('.task-form');
+  var add_task_button_Elem = $('.add-task-button');
+  var task_name_Elem = $('#task-name__field');
+  var task_description_Elem = $('#task-description__field');
+  var empty_list_Elem = $('.empty-list');
+  var task_list_Elem = $('.task-list');
 
-    //Проверка содержимого поля #task-name__field
-    if ($('#task-name__field').val() == '') {
-      alert('Заполните имя задачи');
-      return false;
-    };
-
-    //Проверка содержимого поля #task-description__field
-    if ($('#task-description__field').val() == '') {
-      alert('Заполните описание задачи');
-      return false;
-    };
+  add_task_button_Elem.on('click', function(){
 
     //Записываю содержимое полей в переменные
-    var task_name = $('#task-name__field').val();
-    var task_description = $('#task-description__field').val();
+    let task_name = task_name_Elem.val();
+    let task_description = task_description_Elem.val();
   
     //Скрываю пустой лист
-    $('.empty-list').addClass('hidden');
+    empty_list_Elem.addClass('hidden');
     
     //Генерирую элементы HTML
-    $('.task-list').append('<li class="task-list__item"></li>');
-    $('.task-list__item').last().append('<div class="task-item"></div>');
-    $('.task-item').last().append('<div class="task-item__name-container flex-container"></div>');
-    $('.task-item__name-container.flex-container').last().append('<div class="flex-container"></div>');
-    $('.flex-container').last().append('<div class="task-item__name"></div>');
-    $('.flex-container').last().append('<button class="task-item__clear" type="button" aria-label="Удалить дело"></button>');
-    $('.task-item__name-container.flex-container').last().append('<button class="task-item__open" type="button" aria-label="Раскрыть дело"></button>');
-    $('.task-item').last().append('<div class="task-item__description"></div>');
+    task_list_Elem.append('<li class="task-list__item"></li>');
+    $('.task-list__item').last()
+      .append('<article class="task-item"></article>');
+    $('.task-item').last()
+      .append('<header class="task-item__header flex-container"></header>')
+      .append('<p class="task-item__description"></p>');
+    $('.task-item__header').last()
+      .append('<div class="flex-container"></div>')
+      .append('<button class="task-item__open" type="button" aria-label="Раскрыть дело"></button>');
+    $('.flex-container').last()
+      .append('<h3 class="task-item__name"></h3>')
+      .append('<button class="task-item__delete" type="button" aria-label="Удалить дело"></button>');
 
     //Вставляю содержимое полей в сгенерированные элементы
     $('.task-item__name').last().text(task_name);
     $('.task-item__description').last().text(task_description);
 
     //Очищаю форму
-    $('#task-name__field').val('');
-    $('#task-description__field').val('');
-    
+    task_form_Elem[0].reset();
+
   });
 
-  $('.task-list').on('click', '.task-item__clear', function(){
+  //Клик на кнопке удаления задачи
+  $('.task-list').on('click', '.task-item__delete', function(){
     $(this).closest('.task-list__item').remove();
   });
 
