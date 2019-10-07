fca63176292a3c6f76197d713cb256a18144ac42
ViktorRybakov <viktor.rybakov@gmail.com>
2019-10-07 12:08:08 +0300

    Исправил замечания: Ошибка в верстке Удалил лишнюю обертку JS Изменил именование переменных на camelCase JS Использовал интерполяцию строк JS Переписал свертывание задачи в один обработчик: toggleClass и slideToggle

--- js/script.js
+++ js/script.js
@@ -1,72 +1,60 @@
-$(function(){
+$(function() {
+  var taskFormElem = $(".js-task-form");
+  var taskNameElem = $(".js-task-name__field");
+  var taskDescriptionElem = $(".js-task-description__field");
+  var emptyListElem = $(".js-empty-list");
+  var taskListElem = $(".js-task-list");
 
-  var task_form_Elem = $('.js-task-form');
-  var task_name_Elem = $('.js-task-name__field');
-  var task_description_Elem = $('.js-task-description__field');
-  var empty_list_Elem = $('.js-empty-list');
-  var task_list_Elem = $('.js-task-list');
-
-  task_form_Elem.on('submit', function(){
-
+  taskFormElem.on("submit", function() {
     //Записываю содержимое полей в переменные
-    let task_name = task_name_Elem.val();
-    let task_description = task_description_Elem.val();
-  
-    //Скрываю пустой лист
-    empty_list_Elem.addClass('hidden');
-    
-    //Генерирую элементы HTML
-    task_list_Elem.append('<li class="task-list__item js-task-list__item"></li>');
-    $('.js-task-list__item').last()
-      .append('<article class="task-item js-task-item"></article>');
-    $('.js-task-item').last()
-      .append('<header class="task-item__header js-task-item__header flex-container"></header>')
-      .append('<p class="task-item__description js-task-item__description"></p>');
-    $('.js-task-item__header').last()
-      .append('<div class="flex-container js-flex-container"></div>')
-      .append('<button class="task-item__open js-task-item__open" type="button" aria-label="Раскрыть дело"></button>');
-    $('.js-flex-container').last()
-      .append('<h3 class="task-item__name js-task-item__name"></h3>')
-      .append('<button class="task-item__delete js-task-item__delete" type="button" aria-label="Удалить дело"></button>');
+    var taskName = taskNameElem.val();
+    var taskDescription = taskDescriptionElem.val();
 
-    //Вставляю содержимое полей в сгенерированные элементы
-    $('.task-item__name').last().text(task_name);
-    $('.task-item__description').last().text(task_description);
+    //Скрываю пустой лист
+    emptyListElem.addClass('hidden');
+
+    //Генерирую элементы HTML
+    taskListElem.append('<li class="task-list__item js-task-list__item"></li>');
+    $('.js-task-list__item')
+      .last()
+      .append('<article class="task-item js-task-item"></article>');
+    $('.js-task-item')
+      .last()
+      .append('<header class="task-item__header js-task-item__header flex-container"></header>')
+      .append(`<p class="task-item__description js-task-item__description">
+        ${taskDescription}
+        </p>`);
+    $('.js-task-item__header')
+      .last()
+      .append(`<h3 class="task-item__name js-task-item__name">
+        ${taskName}
+        </h3>`)
+      .append('<button class="task-item__delete js-task-item__delete" type="button" aria-label="Удалить дело"></button>')
+      .append('<button class="task-item__open js-task-item__open" type="button" aria-label="Свернуть описание"></button>');
 
     //Очищаю форму
-    task_form_Elem[0].reset();
+    taskFormElem[0].reset();
 
     event.preventDefault();
-
   });
 
-  //Клик на кнопке удаления задачи
-  task_list_Elem.on('click', '.js-task-item__delete', function(){
-    $(this).closest('.js-task-list__item').remove();
+  //Удаление задачи
+  taskListElem.on('click', '.js-task-item__delete', function() {
+    $(this)
+      .closest('.js-task-list__item')
+      .remove();
   });
 
   //Cворачивание задачи
-  task_list_Elem.on('click', '.js-task-item__open', function(){
-    $(this).addClass('rotate');
-    $(this).closest('.js-task-item').children('.task-item__description')
-      .animate({
-        'height': 0,
-        'min-height': 0,
-        'padding-top': 0,
-        'padding-bottom': 0
-      }, 300);
-  });
+  taskListElem.on('click', '.js-task-item__open', function() {
+    $(this).toggleClass('rotate');
+    $(this)
+      .closest('.js-task-item')
+      .children('.task-item__description')
+      .slideToggle(300, function(){
 
-  //Разворачивание задачи
-  task_list_Elem.on('click', '.js-task-item__open.rotate', function(){
-    $(this).removeClass('rotate');
-    $(this).closest('.js-task-item').children('.task-item__description')
-      .animate({
-        'height': '100%',
-        'min-height': '85px',
-        'padding-top': '10px',
-        'padding-bottom': '10px'
-      }, 300);
+      });
+
   });
 
 });
